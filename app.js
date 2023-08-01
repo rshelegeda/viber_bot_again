55aac9f5a5bc011dc5aa9535a68b8d4f9ed3fee8
Roman Shell <rshelegeda@ukr.net>
2023-08-01 21:21:14 +0300

    123

--- app.js
+++ app.js
@@ -12,28 +12,32 @@ const bot = new ViberBot({
     authToken: botToken,
     name: 'MyViberBot',
     avatar: 'URL_К_ВАШЕЙ_АВАТАРКЕ_БОТА',
-  });
-  
-  // Обработчик события нового сообщения
-  bot.on(BotEvents.MESSAGE_RECEIVED, (message, response) => {
+});
+
+// Обработчик события нового сообщения
+bot.on(BotEvents.MESSAGE_RECEIVED, (message, response) => {
     console.log('Message');
     // Отвечаем на каждое сообщение пользователя текстом "Привет!"
     response.send(new TextMessage('Привет!'));
-  });
-  
-  // Запускаем вебхук на определенном порту
-  
-  const app = express();
-  app.use(express.json());
-  app.post('/webhook', bot.middleware());
-  const port = 3000;
-  app.listen(port, () => {
+});
+
+// Запускаем вебхук на определенном порту
+
+const app = express();
+app.use(express.json());
+app.post('/webhook', bot.middleware());
+const port = 3000;
+app.listen(port, () => {
     console.log(`Бот слушает порт ${port}`);
-  
+
     // Устанавливаем вебхук после того, как сервер запущен и прослушивает порт
-    bot.setWebhook(`https://webhook.site/db1e4d01-55c1-4021-93ef-e6fcb7f97ba9/webhook`).catch((error) => {
-      console.error('Ошибка установки вебхука:', error);
+    // bot.setWebhook(`https://webhook.site/db1e4d01-55c1-4021-93ef-e6fcb7f97ba9`).catch((error) => {
+    //   console.error('Ошибка установки вебхука:', error);
+    // });
+
+    bot.setWebhook('https://91.206.179.119:3000/webhook').catch((error) => {
+        console.error('Ошибка установки вебхука:', error);
     });
-  });
+});
 
   //
